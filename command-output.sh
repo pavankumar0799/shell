7ac14efb82229897068fb8f81d8ab08efd1b1c27
pavankumar <rpavankumar7382@gmail.com>
2025-04-08 21:38:46 +0530

    array

--- command-output.sh
+++ command-output.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-Date=$(date +%f)
+#Date=$(date +%f)
 
-echo " today date is: $Date "
+#echo " today date is: $Date "
 
-#DATE=$(date +%F)
+DATE=$(date +%F)
 
-#echo "Today's date: $DATE"
+echo "Today's date: $DATE"
