cd90ef58862ec2e3c6876f3d399abf0b64dfd295
pavankumar <rpavankumar7382@gmail.com>
2025-04-08 21:42:43 +0530

    array

--- addition.sh
+++ addition.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-number1=$1
-number2=$2
+# number1=$1
+# number2=$2
 
-sum=$((number1 + number1))
+# sum=$((number1 + number1))
 
-echo "total : $sum"
+# echo "total : $sum"
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
