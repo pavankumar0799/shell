55fa5839f96c520264a3670fdd6f0c34ceb5baab
pavankumar <rpavankumar7382@gmail.com>
2025-04-08 18:27:25 +0530

    conversion

--- helloword.sh
+++ helloword.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo " hey hoe are you "
+echo " hey how are you "
+
+person=pavan
+person=sumanth
+
+echo " hi {person1} , how are you "
+echo " i am fine {person2} , what are you doing "
+echo " nothing {person1} "
+echo " ok good {person2}
