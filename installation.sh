00af7a2bae6f9ea3f5d77b98f452f6de27dac1ca
pavankumar <rpavankumar7382@gmail.com>
2025-04-09 21:42:14 +0530

    shell

--- installation.sh
+++ installation.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-userid=$(id -u)
-if [ $userid -ne 0 ]
-then 
- echo " run with root "
- exit 1
+# userid=$(id -u)
+# if [ $userid -ne 0 ]
+# then 
+#  echo " run with root "
+#  exit 1
 
+# fi
+
+# dnf install git -y
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root priveleges"
+    exit 1
 fi
 
-dnf install git -y
+dnf install git -y 
